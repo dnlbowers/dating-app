1453f397cf281fc87c87a161360f5e7ca83be2e4
David Bowers <dbowe@greentube.com>
2022-12-23 09:21:21 +0100

    create route gaurd

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { MembersListComponent } from './members/members-list/members-list.compon
 import { MembersDetailComponent } from './members/members-detail/members-detail.component'
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MembersListComponent},
+  {path: 'members', component: MembersListComponent, canActivate: [AuthGuard]},
   {path: 'members/:id', component: MembersDetailComponent},
   {path: 'lists', component: ListsComponent},
   {path: 'messages', component: MessagesComponent},
