7f3dff967205f799b8a2294ebe0eb410d4c09842
David Bowers <dbowe@greentube.com>
2022-12-26 13:47:12 +0100

    create automapper helper and add to services extension

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,7 +12,6 @@ namespace API.Controllers
         public UsersController(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-
         }
 
         [HttpGet]
