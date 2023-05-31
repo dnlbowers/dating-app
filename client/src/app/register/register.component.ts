d0d0a25d5129ba260ddf82e0c1186510800e7b58
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-05-31 23:36:28 +0200

    expand the registration form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -17,14 +17,18 @@ export class RegisterComponent implements OnInit {
   constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.initializedForm()
+    this.initializeForm()
   }
 
-  initializedForm() {
+  initializeForm() {
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
-      password: ['', [Validators.required, 
-        Validators.minLength(4), Validators.maxLength(8)]],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
     this.registerForm.controls['password'].valueChanges.subscribe({
